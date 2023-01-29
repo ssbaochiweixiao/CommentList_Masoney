215ca38b144388bcdfb9000248be023df5bb55ee
钰雪儿 <673658917@qq.com>
2023-01-29 17:28:05 +0800

    开发一个新的功能-完成了30%

---
+++ day1_FriendCircleDemo/ACAliceViewController.m
@@ -0,0 +1,34 @@
+//
+//  ACAliceViewController.m
+//  day1_FriendCircleDemo
+//
+//  Created by ios_team on 2023/1/29.
+//  Copyright © 2023 Alice_ss. All rights reserved.
+//
+
+#import "ACAliceViewController.h"
+
+@interface ACAliceViewController ()
+
+@end
+
+@implementation ACAliceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    // 这里是注释0
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
