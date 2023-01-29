168915c87a783f6c4fa48453378cd6a3824f0335
钰雪儿 <673658917@qq.com>
2023-01-29 15:57:53 +0800

    test1 2

--- day1_FriendCircleDemo/AppDelegate.m
+++ day1_FriendCircleDemo/AppDelegate.m
@@ -4,7 +4,7 @@
 //
 //  Created by lianshuo on 2020/3/24.
 //  Copyright © 2020 Alice_ss. All rights reserved.
-//  进行修改注释
+//  test1
 
 #import "AppDelegate.h"
 
--- day1_FriendCircleDemo/ViewController.m
+++ day1_FriendCircleDemo/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by lianshuo on 2020/3/24.
 //  Copyright © 2020 Alice_ss. All rights reserved.
-//
+//  test2
 
 #import "ViewController.h"
 
